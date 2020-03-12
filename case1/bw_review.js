8834f27c2a0c2a67d24b3bfdea06ab278420361e
Jeremiah RUiz <Jruiz181@west-mec.org>
2020-03-12 15:26:47 -0700

    finshed case 1

--- case1/bw_review.js
+++ case1/bw_review.js
@@ -33,23 +33,57 @@
       within textStr
 
 */
-window.onload = init();
-
+window.onload = init;
+//init function 
 function init(){
-   var stars = document.querySelectorAll("span#stars");
+   var stars = document.querySelectorAll("span#stars img");
    for(var i = 0; i < stars.length; i++){
       stars[i].style.cursor = "pointer";
-      stars[i].addEventListener("mouseenter", lightStars())
+      stars[i].addEventListener("mouseenter", lightStars)
    }
-   document.getElementById("comment").addEventListener("keyup", Count());
+   document.getElementById("comment").addEventListener("keyup", updateCount);
 }
-
-function lightStars(){
-   
+//lightstars highlights the stars and aloows us to vote the amount of stars
+function lightStars(e){
+   var starNumber = e.target.alt;
+   var stars = document.querySelectorAll("span#stars img");
+   for(var i =0; i< starNumber; i++){
+      stars[i].src ="bw_star2.png";
+   }
+   for(var j = starNumber; j < 5; j++){
+      stars[j].src = "bw_star.png";
+   }
+   //this makes it so that when the mouse leaves the stars they unhiglight
+   document.getElementById("rating").value = starNumber + " stars";
+   e.target.addEventListener("mouseleave", turnOffStars);
+   //when you click on the star, it removes the event listener of turn off stars which keeps the color there to see how many stars you rate the movie
+   e.target.addEventListener("click", function(){
+      e.target.removeEventListener("mouseleave", turnOffStars);
+   })
 }
-
-
-  
+//this function turns off the stars
+function turnOffStars(){
+   var stars = document.querySelectorAll("span#stars img");
+   for(var i = 0; i<stars.length; i++){
+      stars[i].src = "bw_star.png"
+   }
+   document.getElementById("rating").value = "";
+}
+  //counts the words and if goes over it will go red
+function updateCount(){
+   var commentText = document.getElementById("comment").value;
+   var charCount = countCharacters(commentText);
+   var wordCountBox = document.getElementById("wordCount");
+   wordCountBox.value = charCount + "/1000";
+   if(charCount > 1000){
+      wordCountBox.style.background = "red";
+      wordCount.style.color = "white";
+   }
+   else{
+      wordCount.style.color = "black";
+      wordCountBox.style.backgroundColor = "white";
+   }
+}
 
 
   
